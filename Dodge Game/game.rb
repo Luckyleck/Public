c726ce607b271c8f4b37cede0d1de9eb8eabddcb
Luckyleck <118956261+Luckyleck@users.noreply.github.com>
2022-12-31 14:43:31 -0500

    My sandbox

---
+++ Dodge Game/game.rb
@@ -0,0 +1,20 @@
+### Sky Fall Game
+
+$LOAD_PATH << '.'
+require 'assets.rb'
+require 'methods.rb'
+
+
+# Game Script
+
+
+begin_text
+play_round
+
+
+# TO DO, SET DIFFERENT DIFFICULTIES
+
+
+
+
+    
